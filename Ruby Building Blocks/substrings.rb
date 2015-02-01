5bb36494f141c60bac9b56b22951d34daf1ec1a5
Kevin Weir <kevin@kbweir.com>
2015-02-01 13:05:54 -0500

    Finish Substrings

--- Ruby Building Blocks/substrings.rb
+++ Ruby Building Blocks/substrings.rb
@@ -1,14 +1,26 @@
 def substrings (text, substring_array)
 	match = {}
-	word = text.split(/\s+/).inspect
+	word = text.downcase.split(' ')
+	count = 0
 	substring_array.each do |sub|
 		word.each do |word_match|
-			
+			if word_match.include? sub
+				if match.has_key?(sub)
+					match[sub] = count += 1
+				else
+					match[sub] = count = 1
+				end
+			end
 		end
+		
 	end
-	return word
+	return match
 end
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit", "be"]
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-puts substrings("below me", dictionary)
+puts ''
+puts substrings("below", dictionary)
+puts ''
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+puts ''
