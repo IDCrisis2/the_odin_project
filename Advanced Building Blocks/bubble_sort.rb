8acd8fa8512340014cac0179d01ff397a6b58704
Kevin Weir <kevin@kbweir.com>
2015-02-15 14:22:15 -0500

    finish bubble_sort

--- Advanced Building Blocks/bubble_sort.rb
+++ Advanced Building Blocks/bubble_sort.rb
@@ -16,8 +16,28 @@ def bubble_sort(array)
 	return "Sorted array: #{array.inspect}"
 end
 
-def bubble_sort_by(array)
-	
+def bubble_sort_by(unsorted_array)
+    swapped = true
+    while swapped
+        swapped = false
+        (0...unsorted_array.size-1).each do |i|
+            if yield(unsorted_array[i], unsorted_array[i+1]) < 0 
+                holder = unsorted_array[i+1]
+                unsorted_array[i+1] = unsorted_array[i]
+                unsorted_array[i] = holder
+                swapped = true
+            end
+        end
+    end
+    return unsorted_array
 end
 
 puts bubble_sort([4,3,78,2,0,2])
+
+array_to_sort_by = ["hi", "hello", "hey"]
+
+bubble_sort_by(array_to_sort_by) do |left,right|
+    right.length - left.length
+end
+
+puts array_to_sort_by.inspect
