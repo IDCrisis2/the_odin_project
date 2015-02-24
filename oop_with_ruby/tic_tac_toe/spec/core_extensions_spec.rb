da7031a755c2fc9779f2a506f4c0173736ee4e84
Kevin Weir <kevin@kbweir.com>
2015-02-23 19:12:06 -0500

    add and change files

--- oop_with_ruby/tic_tac_toe/spec/core_extensions_spec.rb
+++ oop_with_ruby/tic_tac_toe/spec/core_extensions_spec.rb
@@ -16,5 +16,37 @@ describe Array do
 		end
 	end
 
-	
+	context "#all_same?" do
+		it "returns true if all elememnts of the Array are the same" do
+			expect(["A", "A", "A"].all_same?).to eq true
+		end
+
+		it "returns false if some of the Array elements are not the same" do
+			expect(["", 1, "", Object.new, :a].all_same?).to eq false
+		end
+
+		it "returns true for an empty Array" do
+			expect([].all_same?).to eq true
+		end
+	end
+
+	context "#any_empty?" do
+		it "returns true if any elements of the Array are empty" do
+			expect(["", "A", 1].any_empty?).to eq true
+		end
+
+		it "returns false if none of the elements of the Array are empty" do
+			expect(["A", "B", 2].any_empty?).to eq false
+		end
+	end
+
+	context "#none_empty?" do
+		it "returns true if none of the elements of the Array are empty" do
+			expect(["A", "B", "C"].none_empty?).to eq true
+		end
+
+		it "returns false if any of the elements of the Array are empty" do
+			expect(["", "B", "C"].none_empty?).to eq false
+		end
+	end
 end
